fadfb48ca6147a3094d49e371dc6d80c1cfb5864
ewanld <ewaaan@gmail.com>
2015-07-23 10:25:29 +0200

    changed Predicate to abstract class

--- src/com/github/stream4j/Predicate.java
+++ src/com/github/stream4j/Predicate.java
@@ -1,4 +1,48 @@
 package com.github.stream4j;
-public interface Predicate<T> {
-	boolean test(T t);
+
+public abstract class Predicate<T> {
+	public abstract boolean test(T t);
+
+	public Predicate<T> isEqual(final Object targetRef) {
+		return new Predicate<T>() {
+			@Override
+			public boolean test(T t) {
+				return t.equals(targetRef);
+			}
+		};
+	}
+
+	public Predicate<T> negate() {
+		final Predicate<T> that = this;
+		return new Predicate<T>() {
+
+			@Override
+			public boolean test(T t) {
+				return !that.test(t);
+			}
+		};
+	}
+
+	public Predicate<T> and(final Predicate<? super T> other) {
+		final Predicate<T> that = this;
+		return new Predicate<T>() {
+
+			@Override
+			public boolean test(T t) {
+				return that.test(t) && other.test(t);
+			}
+		};
+
+	}
+
+	public Predicate<T> or(final Predicate<? super T> other) {
+		final Predicate<T> that = this;
+		return new Predicate<T>() {
+
+			@Override
+			public boolean test(T t) {
+				return that.test(t) || other.test(t);
+			}
+		};
+	}
 }
